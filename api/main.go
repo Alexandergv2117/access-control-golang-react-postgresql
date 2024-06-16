15166257e776b7fc5775a64d5a239127c94e621f
Alexander Garcia <alexandergv2117@gmail.com>
2024-06-15 21:42:35 -0500

    feat(api-users): create crud for users

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/Alexandergv2117/access-control-golang-react-postgresql/db"
+	"github.com/Alexandergv2117/access-control-golang-react-postgresql/models"
 	"github.com/Alexandergv2117/access-control-golang-react-postgresql/routes"
 	"github.com/gorilla/mux"
 )
@@ -11,9 +12,15 @@ import (
 func main() {
 	db.DBConnection()
 
+	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler).Methods("GET")
+	r.HandleFunc("/user", routes.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/user/{id}", routes.GetUserHandler).Methods("GET")
+	r.HandleFunc("/user", routes.CreateUserHandler).Methods("POST")
+	r.HandleFunc("/user/{id}", routes.DeleteUserHandler).Methods("DELETE")
 
 	http.ListenAndServe(":5000", r)
 }
