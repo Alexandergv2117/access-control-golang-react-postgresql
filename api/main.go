1749f07e5a746789ea839a2006477cf077d73af7
Alexander Garcia <alexandergv2117@gmail.com>
2024-06-15 21:07:51 -0500

    chore(root): config db connection

--- api/main.go
+++ api/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/Alexandergv2117/access-control-golang-react-postgresql/db"
 	"github.com/Alexandergv2117/access-control-golang-react-postgresql/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db.DBConnection()
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler).Methods("GET")
